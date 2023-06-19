281fdba51263c07d813d3f3134b71d5111994925
Dary <Dafna_pelmeshka@mail.ru>
2023-06-20 02:59:08 +0300

    New tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,27 +4,52 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CashbackHackServiceTest {
-    @Test
+    @org.junit.Test
     public void testRemainLow(){
         CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(100,service.remain(900));
     }
 
-    @Test
+    @org.junit.Test
     public void testRemainHigh(){
         CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(450,service.remain(1550));
     }
 
-    @Test
+    @org.junit.Test
     public void testRemainEquals(){
         CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(0,service.remain(1000));
     }
 
-    @Test
+    @org.junit.Test
     public void testRemainZero(){
         CashbackHackService service = new CashbackHackService();
         Assert.assertEquals(1000,service.remain(0));
     }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainLow2(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(100,service.remain(900));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainHigh2(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(450,service.remain(1550));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainEquals2(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(0,service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainZero2(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(1000,service.remain(0));
+    }
+
 }
