f1f10c3855d44a0b4b59d5c880ff6d20b0af438f
Dary <Dafna_pelmeshka@mail.ru>
2023-06-19 23:33:11 +0300

    Tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testRemainLow(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(100,service.remain(900));
+    }
+
+    @Test
+    public void testRemainHigh(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(450,service.remain(1550));
+    }
+
+    @Test
+    public void testRemainEquals(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(0,service.remain(1000));
+    }
+
+    @Test
+    public void testRemainZero(){
+        CashbackHackService service = new CashbackHackService();
+        Assert.assertEquals(1000,service.remain(0));
+    }
+}
